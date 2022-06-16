ae621e97b7eebfa2ab947ed82399235539b41605
Tsyriulnikov <max_djvu@mail.ru>
2022-06-16 16:02:27 +0300

    issue01

--- Contains_Duplicate.js
+++ Contains_Duplicate.js
@@ -1,3 +1,25 @@
+
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+*/
+
+
+
 var containsDuplicate = function(array) {
   var valuesSoFar = Object.create(null);
     for (var i = 0; i < array.length; ++i) {
@@ -9,3 +31,9 @@ var containsDuplicate = function(array) {
     }
     return false;
 };
+
+или
+function containsDuplicate(a) {
+    const noDups = new Set(a);
+     return a.length !== noDups.size;
+  }
